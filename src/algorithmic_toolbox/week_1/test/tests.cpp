5757418b582e83f3341e4ab9818f4d98769f544f
Mostafa,Ziad <ziad.mostafa@zf.com>
2020-05-15 02:33:08 +0200

    define vector as a refenernce

--- src/algorithmic_toolbox/week_1/test/tests.cpp
+++ src/algorithmic_toolbox/week_1/test/tests.cpp
@@ -4,8 +4,8 @@
 
 TEST_CASE("Testing Max pair Product")
 {
-  REQUIRE(MaxPairwiseProduct(std::vector<int> {1,2,3}) == 6);
-  REQUIRE(MaxPairwiseProduct(std::vector<int> {100000,90000}) == 9000000000);
-  REQUIRE(MaxPairwiseProduct(std::vector<int> {4,5,6,10}) == 60);
-  REQUIRE(MaxPairwiseProduct(std::vector<int> {5,10,15,20}) == 300);
+  REQUIRE(MaxPairwiseProduct(std::vector<int>& {1,2,3}) == 6);
+  REQUIRE(MaxPairwiseProduct(std::vector<int>& {100000,90000}) == 9000000000);
+  REQUIRE(MaxPairwiseProduct(std::vector<int>& {4,5,6,10}) == 60);
+  REQUIRE(MaxPairwiseProduct(std::vector<int>& {5,10,15,20}) == 300);
 }
